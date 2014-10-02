f7fdf2eca7a83fbe7c03d23e17532d0febf7cff3
cesarpachon <cesarpachon@gmail.com>
2014-10-02 11:53:53 -0500

    green test for instantiation

--- specs/test01_spec.js
+++ specs/test01_spec.js
@@ -5,15 +5,8 @@ var MAX_GLOBAL_SIZE = 1000;
 var MAX_PAGE_SIZE = 100;
 var LOCAL_BUFFER_SIZE = 3; //num of pages
 
-/**
-* provider is a callback function that is used as source for a remote large buffer.
-* @param skip {Number} current global position of the iterator
-* @param max_mage_size {Number} max page size requested. may return less.
-* @param cb {Function} callback that is invoked when new data is ready. it pass and array of records. empty if no more.
-*/
+
 var provider = function(skip, max_page_size, cb){
-
-
 };
 
 /**
@@ -28,9 +21,6 @@ it("should know global size", function(){
 });
 
 
-/**
-* the local buffer must have a constant size of LOCAL_BUFFER_SIZE * MAX_PAGE_SIZE
-*/
 it("should reserve local slots", function(){
   expect(ibuffer.slots.length).toBe(LOCAL_BUFFER_SIZE*MAX_PAGE_SIZE);
 });
--- src/infinite-buffer.js
+++ src/infinite-buffer.js
@@ -4,12 +4,25 @@ var InfiniteBuffer = (function(){
 
   /**
   * @constructor
+  * @param max_global_size {Number} total size of the target buffer
+  * @param max_page_size {Number} max size for pagination calls
+  * @param local_buffer_size {Number} size of the local buffer, as factor of max_page_size (eg: 3 means 3*max_page_size slots)
+   @param provider {function }  is a callback function that is used as source for a remote large buffer.
+      * @param skip {Number} current global position of the iterator
+      * @param max_mage_size {Number} max page size requested. may return less.
+      * @param cb {Function} callback that is invoked when new data is ready. it pass and array of records. empty if no more.
   */
- var InfiniteBuffer = function(){
+ var InfiniteBuffer = function(global_size, max_page_size, local_buffer_size, provider){
+   this.global_size = global_size;
+   this.max_page_size = max_page_size;
+   this.slots = new Array(local_buffer_size*max_page_size);
+   this.provider = provider;
+   this.index = 0;
+   this.head = 0;
+   this.tail = 0;
  };
 
-
-  return InfiniteBuffer;
+ return InfiniteBuffer;
 
 
 })();
