02384306899c202f94aa68693194da2c0cd1d95b
cesarpachon <cesarpachon@gmail.com>
2014-10-03 11:23:29 -0500

    adding green test

--- specs/spec_spec.js
+++ specs/spec_spec.js
@@ -1,12 +1,22 @@
 describe("infinitebuffer", function(){
 
+var i, sum, count;
+
 //this is the size of our large buffer
 var MAX_GLOBAL_SIZE = 1000;
 var MAX_PAGE_SIZE = 100;
-var LOCAL_BUFFER_SIZE = 3; //num of pages
 
 
+  /**
+  * simple provider that will return data[i] = skip + i up to MAX_GLOBAL_SIZE
+  */
 var provider = function(skip, max_page_size, cb){
+  var max = Math.min(MAX_GLOBAL_SIZE, skip+max_page_size);
+  data = [];
+  for(i=0; i<max; ++i){
+    data.push(skip+i);
+  }
+  cb(data);
 };
 
 var ibuffer = null;
@@ -15,7 +25,7 @@ var ibuffer = null;
   describe("instantiation", function(){
 
     beforeEach(function(){
-      ibuffer = new InfiniteBuffer(MAX_GLOBAL_SIZE, MAX_PAGE_SIZE, LOCAL_BUFFER_SIZE, provider);
+      ibuffer = new InfiniteBuffer(MAX_GLOBAL_SIZE, MAX_PAGE_SIZE, provider);
     });
 
     it("should know global size", function(){
@@ -23,27 +33,45 @@ var ibuffer = null;
     });
 
     it("should reserve local slots", function(){
-      expect(ibuffer.slots.length).toBe(LOCAL_BUFFER_SIZE*MAX_PAGE_SIZE);
+      expect(ibuffer.slots.length).toBe(MAX_PAGE_SIZE);
     });
 
-    it("should start at index 0", function(){
-      expect(ibuffer.index).toBe(0);
-      expect(ibuffer.head).toBe(0);
-      expect(ibuffer.tail).toBe(0);
+    it("should start at indexes -1", function(){
+      expect(ibuffer.local_index).toBe(-1);
+      expect(ibuffer.page).toBe(-1);
+      expect(ibuffer.slots_lenght).toBe(0);
     });
 
   });
 
-  describe("iteration", function(){
+  describe("should support forward iteration", function(){
 
     beforeEach(function(){
-      ibuffer = new InfiniteBuffer(MAX_GLOBAL_SIZE, MAX_PAGE_SIZE, LOCAL_BUFFER_SIZE, provider);
+      ibuffer = new InfiniteBuffer(MAX_GLOBAL_SIZE, MAX_PAGE_SIZE, provider);
     });
 
-    it("should retrive first page of data", function(){
-      expect(ibuffer.next()).toBe();
+    it("should iterate over all the data", function(){
+      sum = 0;
+      count = 0;
+      while(ibuffer.hasGlobalNext()){
+        count++;
+        //important! the number of calls to "next" are more than MAX_GLOBAL_SIZE,
+        //because for each page, we invoke next twice.
+        if(count > MAX_GLOBAL_SIZE + (MAX_GLOBAL_SIZE/MAX_PAGE_SIZE)){
+          throw "something went wrong with this test: infinite loop?";
+        }
+        if(ibuffer.next()){
+          sum += ibuffer.val();
+        }else{
+          //waiting for remote data!
+        }
+      }
 
+      var numints = MAX_GLOBAL_SIZE -1;
+      expect(sum).toBe((numints*(numints+1))/2);
+      //expect(sum).toBe(MAX_GLOBAL_SIZE);
     });
+
 
 
   });
--- src/infinite-buffer.js
+++ src/infinite-buffer.js
@@ -12,15 +12,101 @@ var InfiniteBuffer = (function(){
       * @param max_mage_size {Number} max page size requested. may return less.
       * @param cb {Function} callback that is invoked when new data is ready. it pass and array of records. empty if no more.
   */
- var InfiniteBuffer = function(global_size, max_page_size, local_buffer_size, provider){
+ var InfiniteBuffer = function(global_size, max_page_size, provider){
    this.global_size = global_size;
    this.max_page_size = max_page_size;
-   this.slots = new Array(local_buffer_size*max_page_size);
+   this.slots = new Array(this.max_page_size);
+   //usually will be equals to slots.length (max_page_size) but sometimes it may be less (i.e, at the end of the global buffer)
+   this.slots_lenght = 0;
    this.provider = provider;
-   this.index = 0;
-   this.head = 0;
-   this.tail = 0;
+   //position of the local buffer related to the global buffer
+   this.page = -1;
+   this.local_index = -1;
+   this.waiting_for_data = false;
  };
+
+
+  /**
+  * return true if there is more data in the global (remote) buffer,
+  * it does not matter if the data is out of range of the local array
+  */
+  InfiniteBuffer.prototype.hasGlobalNext = function(){
+    return (this.local_index+1>=0) &&
+      (this._localToGlobalIndex(this.local_index + 1) < this.global_size);
+  };
+
+
+  /**
+  * try to advance the local index.
+  * return true if the data is available for retrieval with the val method.
+  * return false means that it is waiting for retrieval of remote data from the provider.
+  */
+  InfiniteBuffer.prototype.next = function(){
+
+    if(this.waiting_for_data) {
+      return false;
+    }
+
+    if(this._globalIndexAvailable(this._localToGlobalIndex(this.local_index+1))){
+      this.local_index++;
+      console.log("next true. local_index ", this.local_index, this._localToGlobalIndex(this.local_index));
+
+      return true;
+    }
+    else{
+      this.page++;
+      this._retrievePage();
+      return false;
+    }
+  };
+
+
+  /**
+  * returns the value to which is pointing local_index
+  */
+  InfiniteBuffer.prototype.val = function(){
+    return this.slots[this.local_index];
+  };
+
+
+  /**
+  * returns {Number} global index transformed into global index
+  */
+  InfiniteBuffer.prototype._localToGlobalIndex = function(li){
+    return (this.page*this.max_page_size) + li;
+  };
+
+
+  /**
+  * returns true if global index gi is loaded into local data
+  */
+  InfiniteBuffer.prototype._globalIndexAvailable = function(gi){
+    if(gi < 0) return false;
+    var offset = this.page*this.max_page_size;
+    return gi >= offset && gi < offset + this.max_page_size;
+  };
+
+
+
+
+  /**
+  *
+  */
+  InfiniteBuffer.prototype._retrievePage = function(){
+    this.waiting_for_data = true;
+    var offset = (this.page)*this.max_page_size;
+    var self = this;
+    this.provider(offset, this.max_page_size, function(data){
+      self.slot_length = Math.min(data.length, self.max_page_size);
+      for(var i=0; i<self.slot_length; ++i){
+        self.slots[i] = data[i];
+      }
+      console.log(self.slots);
+      self.waiting_for_data = false;
+      self.local_index = -1;
+    });
+  };
+
 
  return InfiniteBuffer;
 
